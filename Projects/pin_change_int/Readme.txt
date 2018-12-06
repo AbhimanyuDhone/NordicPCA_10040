483ecf0eaf8e2c7a7dc94d9906b7b86fbe2880d9
Abhimanyu D <abhimanyudhone@gmail.com>
2018-12-06 07:13:49 -0500

    Adding ->Basic modules and ->Button event to LED blink example (getting started)

---
+++ Projects/pin_change_int/Readme.txt
@@ -0,0 +1,4 @@
+Project to blink an LED with Button Press
+
+Good Example to get started and understand
+the abstraction levels in the Nordic Board
---
+++ gpiote_test.c
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2014 - 2018, Nordic Semiconductor ASA
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ * 
+ * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * 4. This software, with or without modification, must only be used with a
+ *    Nordic Semiconductor ASA integrated circuit.
+ * 
+ * 5. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+/** @file
+*
+* @defgroup gpiote_example_main main.c
+* @{
+* @ingroup nrf_gpiote_example
+* @brief GPIOTE Example Application main file.
+*
+* This file contains the source code for a sample application using GPIOTE.
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf.h"
+#include "nrf_gpiote.h"
+#include "nrf_gpio.h"
+#include "boards.h"
+#include "nrf_drv_ppi.h"
+#include "nrf_drv_timer.h"
+#include "nrf_drv_gpiote.h"
+#include "app_error.h"
+
+#ifdef BSP_LED_0
+    #define GPIO_OUTPUT_PIN_NUMBER BSP_LED_0  /**< Pin number for output. */
+#endif
+
+#ifdef BUTTON_1
+    #define GPIO_INPUT_PIN_NUMBER BUTTON_1
+#endif
+#ifndef GPIO_OUTPUT_PIN_NUMBER
+    #error "Please indicate output pin"
+#endif
+
+static nrf_drv_timer_t timer = NRF_DRV_TIMER_INSTANCE(0);
+
+void timer_dummy_handler(nrf_timer_event_t event_type, void * p_context){}
+
+static void led_blinking_setup()
+{
+  
+}
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_drv_ppi_init();
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_gpiote_init();
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
+    err_code = nrf_drv_timer_init(&timer, &timer_cfg, timer_dummy_handler);
+    APP_ERROR_CHECK(err_code);
+#ifdef NRF51
+    //Workaround for PAN-73.
+    *(uint32_t *)0x40008C0C = 1;
+#endif
+
+    // Setup PPI channel with event from TIMER compare and task GPIOTE pin toggle.
+    //led_blinking_setup();
+
+    // Enable timer
+    //nrf_drv_timer_enable(&timer);
+    nrf_drv_gpiote_out_config_t config = GPIOTE_CONFIG_IN_SENSE_HITOLO(true);
+    nrfx_gpiote_in_init(GPIO_INPUT_PIN_NUMBER, &config,led_blinking_setup)
+    nrf_ppi_channel_t ppi_channel;
+    err_code = nrf_drv_ppi_channel_alloc(&ppi_channel);
+    APP_ERROR_CHECK(err_code);
+    nrfx_gpiote_in_event_enable(GPIO_INPUT_PIN_NUMBER,1)
+    while (true)
+    {
+        // Do Nothing - GPIO can be toggled without software intervention.
+    }
+}
+
+
+/** @} */
